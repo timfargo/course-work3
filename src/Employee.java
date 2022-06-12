fc44ada911f358e941647f7e936fda6e7d065bea
timfargo <isalintimur981@gmail.com>
2022-06-12 09:56:13 +0300

    курсовая3

---
+++ src/Employee.java
@@ -0,0 +1,45 @@
+public class Employee {
+  private static int counter = 1;
+
+
+    private final String  name;
+    private final String surname;
+    private final String patronymic;
+    private final double salary;
+    private final   int department;
+    private final int id;
+
+    public  Employee(String name,String surname,String patronymic,double salary,int department){
+        this.id = counter++;
+        this.name = name;
+        this.surname = surname;
+        this.patronymic = patronymic;
+        this.salary = salary;
+        this.department = department;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getSurname(){
+        return surname;
+    }
+
+    public String getPatronymic(){
+        return patronymic;
+    }
+
+    public double getSalary(){
+        return salary;
+    }
+
+    public int getDepartment(){
+        return department;
+    }
+
+    public int getId(){
+        return id;
+    }
+}
+
